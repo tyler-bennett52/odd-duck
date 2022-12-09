fb6e7f6e648ec4759225f71bebd9c2a3bc4173ca
TYLER BENNETT <tyler.bennett52@gmail.com>
2022-12-09 04:07:46 -0600

    repo template, wireframe buildout, img assetsarray

---
+++ js/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const imageSourceArray = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.jpg', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/water-can.jpg', 'img/wine=glass.jpg'];
+
