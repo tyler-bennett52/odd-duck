4849ed2b4a32fec791e4783315df898149eb21be
TYLER BENNETT <tyler.bennett52@gmail.com>
2022-12-12 15:09:48 -0600

    game start/stop functionality finished

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 'use strict';
 // const imageSourceArray = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.jpg', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/water-can.jpg', 'img/wine=glass.jpg'];
 const allProducts = [];
-const imgChoices = document.querySelectorAll('img');
 const randomIndices = [0, 0, 0];
 let randomSelection1 = 0;
 let randomSelection2 = 0;
 let randomSelection3 = 0;
+
+// ************************* DOM ITEMS **********************************
+const imgChoices = document.querySelectorAll('img');
+const startBtn = document.querySelector('button');
+
+
+// ************************ HELPER FUNCTIONS ********************************
 const getRandomIndex = () => {
   return Math.floor(Math.random() * allProducts.length);
 };
@@ -55,9 +61,30 @@ console.log(allProducts);
 //   body.appendChild(tempElement);
 // }
 
-randomSelection1 = getRandomIndex();
-randomSelection2 = getRandomIndex();
-randomSelection3 = getRandomIndex();
+function render() {
+  randomSelection1 = getRandomIndex();
+  randomSelection2 = getRandomIndex();
+  while (randomSelection2 === randomSelection1) {
+    randomSelection2 = getRandomIndex();
+  }
+  randomSelection3 = getRandomIndex();
+  while (randomSelection3 === randomSelection2 || randomSelection3 === randomSelection1) {
+    randomSelection3 = getRandomIndex();
+  }
+  imgChoices[0].src = allProducts[randomSelection1].src;
+  imgChoices[1].src = allProducts[randomSelection2].src;
+  imgChoices[2].src = allProducts[randomSelection3].src;
+
+  roundsLeft--;
+  if (roundsLeft === 0) {
+    for (let img of imgChoices) {
+      img.removeEventListener('click', render);
+      img.src = 'https://place-hold.it/300x375/ddd';
+    }
+  }
+}
+
 for (let img of imgChoices) {
-  img.src = allProducts[getRandomIndex()].src;
+  img.addEventListener('click', render);
+
 }
